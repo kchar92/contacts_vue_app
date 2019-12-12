59d98f16b0795d31b17b9abfb0621c048bf67881
Keelana Char <keelanachar@gmail.com>
2019-12-11 21:57:14 -0600

    init, create index and new/create actions for contacts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+
+    <p>First name: <input type="text" v-model="contactFirstName"></p>
+    <p>Email: <input type="text" v-model="contactEmail"></p>
+    <p>Phone Number: <input type="text" v-model="contactPhoneNumber"></p>
+    <button v-on:click="createContact()">Create contact</button>
+
+    <div v-for="contact in contacts">
+      <p>{{ contact.first_name }}</p>
+      <p>{{ contact.email }}</p>
+      <hr>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      message: "Contacts!",
+      contacts: [],
+      contactFirstName: "",
+      contactEmail: "",
+      contactPhoneNumber: ""
+    };
+  },
+  created: function() {
+    console.log('in created');
+    axios.get('/api/contacts').then(response => {
+      console.log(response.data);
+      this.contacts = response.data;
+    });
+  },
+  methods: {
+    createContact: function() {
+      console.log('creating contact');
+      
+      var params = {
+        first_name: this.contactFirstName,
+        email: this.contactEmail,
+        phone_number: this.contactPhoneNumber
+      };
+
+      axios.post('/api/contacts', params).then(response => {
+        console.log(response.data);
+        this.contacts.push(response.data);
+        this.contactFirstName = "";
+        this.contactEmail = "";
+        this.contactPhoneNumber = "";
+      });
+    }
   }
-}
+};
 </script>
