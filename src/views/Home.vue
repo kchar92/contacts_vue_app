d92b6bbc7b5676d55d77a97641fd451726f60faf
Keelana Char <keelanachar@gmail.com>
2019-12-14 20:42:44 -0600

    create edit/update action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,15 +5,30 @@
     <p>First name: <input type="text" v-model="contactFirstName"></p>
     <p>Email: <input type="text" v-model="contactEmail"></p>
     <p>Phone Number: <input type="text" v-model="contactPhoneNumber"></p>
+    <p>Bio: <input type="text" v-model="contactBio"></p>
     <button v-on:click="createContact()">Create contact</button>
+    <hr>
 
     <div v-for="contact in contacts">
       <p>{{ contact.first_name }}</p>
-      <button v-on:click="showInfo(contact)">Show more info</button>
+
+      <button v-on:click="showInfo(contact)">Show more contact info</button>
+      
+
+      
+
       <div v-if="currentContact === contact">
         <p>{{ contact.phone_number }}</p>
-        <p>{{ contact.email }}</p>       
+        <p>{{ contact.email }}</p>
+        <p>{{ contact.bio }}</p>
+
+        <p>First name: <input type="text" v-model="contact.first_name"></p>
+        <p>Email: <input type="text" v-model="contact.email"></p>
+        <p>Phone Number: <input type="text" v-model="contact.phone_number"></p>
+        <p>Bio: <input type="text" v-model="contact.bio"></p>
+        <button v-on:click="updateContact(contact)">Update contact</button>      
       </div>
+
       <hr>
     </div>
   </div>
@@ -32,6 +47,7 @@ export default {
       contactFirstName: "",
       contactEmail: "",
       contactPhoneNumber: "",
+      contactBio: "",
       currentContact: {}
     };
   },
@@ -49,7 +65,8 @@ export default {
       var params = {
         first_name: this.contactFirstName,
         email: this.contactEmail,
-        phone_number: this.contactPhoneNumber
+        phone_number: this.contactPhoneNumber,
+        bio: this.contactBio
       };
 
       axios.post('/api/contacts', params).then(response => {
@@ -67,6 +84,24 @@ export default {
       } else {
         this.currentContact = theContact;
       }
+    },
+    updateContact: function(theContact) {
+      console.log('updating the contact');
+
+      var params = {
+        first_name: theContact.first_name,
+        email: theContact.email,
+        phone_number: theContact.phone_number,
+        bio: theContact.bio
+      };
+
+      axios.patch(`/api/contacts/${theContact.id}`, params).then(response => {
+        console.log(response.data);
+        theContact.first_name = response.data.first_name;
+        theContact.email = response.data.email;
+        theContact.phone_number = response.data.phone_number;
+        theContact.bio = response.data.bio;
+      });
     }
   }
 };
